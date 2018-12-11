133c9115c71d8ab013378066a719d49a11fcb58f
ExceliAdam <adam@exceliadam.pl>
2018-12-11 15:28:39 +0100

    Zmiana nazw klas

--- TestowanieDay1/src/test/java/calculator/CalculatorAppTest.java
+++ TestowanieDay1/src/test/java/calculator/CalculatorAppTest.java
@@ -1,19 +1,18 @@
-package kalkulator;
+package calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 //Zmieniamy nazwę opisu klasy w wynikach testów
-@DisplayName("Kocurek")
-class KalkulatorAppTest {
+//@DisplayName("Kocurek")
+class CalculatorAppTest {
     @Test
     public void addTwoPositiveDoubles() {
         //arrange
-        KalkulatorApp kalkulatorek = new KalkulatorApp();
+        CalculatorApp calc = new CalculatorApp();
         //act
-        double sumad = kalkulatorek.add(1.3, 2.2);
+        double sumad = calc.add(1.3, 2.2);
         //Sprawdzanie z dokładnością
         //Assertions.assertEquals(3.4, sumad, 0.000001, "Sprawdź, że dodawanie liczb double działa");
         //Zmieniliśmy w importach
@@ -24,17 +23,17 @@ class KalkulatorAppTest {
 
 
         assertEquals(3.5, sumad, "Sprawdź, że dodawanie liczb double działa");
-        sumad = kalkulatorek.add(1, 2.3);
+        sumad = calc.add(1, 2.3);
         assertEquals(3.3, sumad, "Sprawdź, że dodawanie liczb double działa");
     }
 
     @Test
     public void addTwoPositiveIntegers() {
         //arrange
-        KalkulatorApp kalkulatorek = new KalkulatorApp();
+        CalculatorApp calc = new CalculatorApp();
 
         //act
-        int suma = kalkulatorek.add(1, 2);
+        int suma = calc.add(1, 2);
 
         //assert
         assertEquals(3, suma, "Sprawdź, że dodawanie liczb całkowitych działa");
@@ -42,8 +41,8 @@ class KalkulatorAppTest {
 
     @Test
     public void divideTwoPositiveDoubles() {
-        KalkulatorApp kalkulatorek = new KalkulatorApp();
-        double divide = kalkulatorek.divide(5, 3);
+        CalculatorApp calc = new CalculatorApp();
+        double divide = calc.divide(5, 3);
         assertEquals(1.6666666666666, divide, 0.0000001, "Sprawdź, że dxielenie liczb zmiennoprzecinkowych działa");
     }
 
